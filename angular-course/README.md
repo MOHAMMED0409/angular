f0f2b0b287a33bb19f620255cb9a2abe2cbdd27a
MOHAMMED0409 <mohammedkhashif02@gmail.com>
2025-01-23 22:49:05 +0530

    typescript-2

--- angular-course/README.md
+++ angular-course/README.md
@@ -1,4 +1,4 @@
-# DemoProject
+# AngularCourse
 
 This project was generated using [Angular CLI](https://github.com/angular/angular-cli) version 19.1.3.
 
--- angular-course/src/app/app.component.spec.ts
+++ angular-course/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'demo-project' title`, () => {
+  it(`should have the 'angular-course' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('demo-project');
+    expect(app.title).toEqual('angular-course');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, demo-project');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, angular-course');
   });
 });

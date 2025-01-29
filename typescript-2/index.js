b18b830ecc347c73473c65c8804da6af51e6413a
mohammed khashif <mohammedkhashif02@gmail.com>
2025-01-29 15:30:03 +0530

    Completed Typescript

--- typescript-2/index.js
+++ typescript-2/index.js
@@ -1,40 +1,64 @@
-// Lesson -2 
+"use strict";
+let cashinregister = 100;
+let nextorderid = 1;
+let nextpizzaid = 1;
 const menu = [
-    {name :"Margherita", price : 8},
-    {name :"Pepperoni", price : 7},
-    {name :"Hawaiian", price : 6},
-    {name :"Veggie", price : 5}
-]
-
-const cashinregister = 100;
-const nextorderid = 1;
+    { id: nextpizzaid++, name: "Margherita", price: 8 },
+    { id: nextpizzaid++, name: "Pepperoni", price: 7 },
+    { id: nextpizzaid++, name: "Hawaiian", price: 6 },
+    { id: nextpizzaid++, name: "Veggie", price: 5 },
+];
 const orderqueue = [];
-
-function addnewpizza(pizzaobj){
-    menu.push(pizzaobj);
+function addnewpizza(pizzaobj) {
+    const newpizza = Object.assign({ id: nextpizzaid++ }, pizzaobj);
+    menu.push(newpizza);
+    return newpizza;
 }
-
-function placeorder(pizzaname){
+function placeorder(pizzaname) {
     const selectpizza = menu.find(pizzaobj => pizzaobj.name === pizzaname);
+    if (!selectpizza) {
+        console.error(`${pizzaname} is not on the menu`);
+        return;
+    }
     cashinregister += selectpizza.price;
-    const neworder = {id :nextorderid++, pizza : selectpizza, status : "ordered"};
+    const neworder = { id: nextorderid++, pizza: selectpizza, status: "ordered" };
     orderqueue.push(neworder);
     return neworder;
 }
-
-function completeorder(orderid){
+function addtoarray(array, item) {
+    array.push(item);
+    return array;
+}
+//example usage:
+addtoarray(menu, { id: nextpizzaid++, name: "Meat Feast", price: 9 });
+addtoarray(orderqueue, { id: nextorderid++, pizza: menu[2], status: "done" });
+console.log(menu);
+console.log(orderqueue);
+function completeorder(orderid) {
     const order = orderqueue.find(order => order.id === orderid);
+    if (!order) {
+        console.error(`${orderid} not found`);
+        return;
+    }
     order.status = "completed";
     return order;
 }
-
-addnewpizza({name :"Meat Feast", price : 9});
-addnewpizza({name :"Vegan", price : 10});
-addnewpizza({name :"BBQ Chicken", price : 11});
-
+function getpizzadetail(identifier) {
+    if (typeof identifier === "string") {
+        return menu.find(pizza => pizza.name.toLowerCase() === identifier.toLowerCase());
+    }
+    else if (typeof identifier === "number") {
+        return menu.find(pizza => pizza.id === identifier);
+    }
+    else {
+        throw new TypeError("Parameter 'indentifier' must be a string or number");
+    }
+}
+addnewpizza({ name: "Meat Feast", price: 9 });
+addnewpizza({ name: "Vegan", price: 10 });
+addnewpizza({ name: "BBQ Chicken", price: 11 });
 placeorder("Margherita");
-completeorder("1");
-
+completeorder(1);
 console.log("Menu:", menu);
 console.log("Cash in register:", cashinregister);
 console.log("order queue:", orderqueue);
--- typescript-2/index.ts
+++ typescript-2/index.ts
@@ -1,4 +1,5 @@
 type pizza = {
+    id : number
     name: string
     price: number
 }
@@ -6,22 +7,29 @@ type pizza = {
 type order = {
     id: number
     pizza: pizza
-    status: string
+    status: "ordered" | "completed"
 }
-
-const menu = [
-    { name: "Margherita", price: 8 },
-    { name: "Pepperoni", price: 7 },
-    { name: "Hawaiian", price: 6 },
-    { name: "Veggie", price: 5 },
-]
 
 let cashinregister: number = 100;
 let nextorderid: number = 1;
+let nextpizzaid: number = 1;
+
+const menu: pizza[] = [
+    { id:nextpizzaid++, name: "Margherita", price: 8 },
+    { id:nextpizzaid++, name: "Pepperoni", price: 7 },
+    { id:nextpizzaid++, name: "Hawaiian", price: 6 },
+    { id:nextpizzaid++, name: "Veggie", price: 5 },
+]
+
 const orderqueue: order[] = [];
 
-function addnewpizza(pizzaobj: pizza) {
-    menu.push(pizzaobj);
+
+function addnewpizza(pizzaobj: Omit<pizza, "id">): pizza {
+    const newpizza: pizza ={
+        id: nextpizzaid++, ...pizzaobj
+    }
+    menu.push(newpizza);
+    return newpizza;
 }
 
 function placeorder(pizzaname: string) {
@@ -31,10 +39,22 @@ function placeorder(pizzaname: string) {
         return;
     }
     cashinregister += selectpizza.price;
-    const neworder = { id: nextorderid++, pizza: selectpizza, status: "ordered" };
+    const neworder:order = { id: nextorderid++, pizza: selectpizza, status: "ordered" };
     orderqueue.push(neworder);
     return neworder;
 }
+
+function addtoarray<T>(array:T[], item: T): T[] {
+    array.push(item);
+    return array;
+}
+
+//example usage:
+addtoarray(menu, { id: nextpizzaid++, name: "Meat Feast", price: 9 });
+addtoarray<order>(orderqueue, { id:nextorderid++, pizza: menu[2], status: "completed" });
+
+console.log(menu);
+console.log(orderqueue)
 
 function completeorder(orderid: number) {
     const order = orderqueue.find(order => order.id === orderid);
@@ -46,6 +66,19 @@ function completeorder(orderid: number) {
     return order;
 }
 
+function getpizzadetail(identifier: string | order): pizza | undefined {
+    if (typeof identifier === "string") {
+        return menu.find(pizza => pizza.name.toLowerCase() === identifier.toLowerCase());
+    }
+
+    else if (typeof identifier === "number") {
+        return menu.find(pizza => pizza.id === identifier);
+    }
+
+    else {
+        throw new TypeError("Parameter 'indentifier' must be a string or number");
+    }
+}
 addnewpizza({ name: "Meat Feast", price: 9 });
 addnewpizza({ name: "Vegan", price: 10 });
 addnewpizza({ name: "BBQ Chicken", price: 11 });

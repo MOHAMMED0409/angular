7416827c571e4e97a8a0344c8c8f53786b1e11b1
mohammed khashif <mohammedkhashif02@gmail.com>
2025-02-17 12:55:48 +0530

    error

--- angular-tut-demo/src/app/app.config.server.ts
+++ angular-tut-demo/src/app/app.config.server.ts
@@ -1,8 +1,9 @@
-// import { InjectionToken } from '@angular/core';
+import { InjectionToken } from '@angular/core';
+import config from '../app/app.config.server';
 
-// export interface AppServiceConfig {
-//   apiUrl: string;
-//   enableLogging: boolean;
-// }
+export interface AppServiceConfig {
+  apiUrl: string;
+  enableLogging: boolean;
+}
 
-// export const APP_SERVICE_CONFIG = new InjectionToken<AppServiceConfig>('app.service.config');
+export const APP_SERVICE_CONFIG = new InjectionToken<AppServiceConfig>('app.service.config');
